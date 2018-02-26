cb0975445ed0ca85cf7786ec5b5ae3a0b71e2e41
Gaia94 <riccardigaia94@gmail.com>
2018-02-26 17:50:46 +0100

    [23/02/2018] Traduzione in inglese LNO (bandiera + auto)

--- index.js
+++ index.js
@@ -49,6 +49,41 @@ app.post('/thankyou', function (req, res) {
     res.sendFile(__dirname + '/_/thankyou.html');
 });
 
+
+
+app.get('/2', function (req, res) {
+
+    let dataUs = fs.readFileSync(__dirname + '/data/en_US.json', 'UTF-8');
+
+    dataUs = JSON.parse(dataUs);
+
+    let wordCount = 0;
+
+    let responseData = '<form method="POST">';
+
+    for (let key in dataUs) {
+        responseData += '<div style="margin-bottom:40px"><input type="checkbox" style="vertical-align:25px; margin-right:15px"><textarea rows="4" cols="50" value="'+ key +'">' + key + '</textarea>';
+        responseData += '<textarea style="margin-left:20px" rows="4" cols="50" name="' + key + '" value="' + dataUs[key] +'">' + dataUs[key] +'</textarea></div>';
+
+        wordCount += dataUs[key].split(' ').length;
+    }
+    responseData += '<div><button type="submit">Salva</button></div>';
+    responseData += '</form>';
+    responseData += 'word count: ' + wordCount;
+
+
+    res.send(responseData);
+
+});
+
+
+app.post('/2', function (req, res) {
+    console.log(req.body);
+});
+
+
+
+
 app.use(express.static('_'));
 
 app.listen(3000, function () {
